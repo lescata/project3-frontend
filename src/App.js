14935495f95bf31f561223f033a0b448ac5116e1
WilsonF78 <fortes.wilson78@gmail.com>
2022-10-01 16:31:48 +0200

    --

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import './App.css';
-import {Routes, Route} from "react-router-dom"
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
