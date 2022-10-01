67d0b778887e51b6c276d563cde30e33e6bd0c41
WilsonF78 <fortes.wilson78@gmail.com>
2022-10-01 18:15:28 +0200

    Route login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./Pages/Homepage"
+import Homepage from "./Pages/Homepage";
+import Login from "./Pages/Login";
 
 function App() {
-  return (<div className="App">
-  <Routes>
-    <Route path="/" element={<Homepage />} />
-  </Routes>
-
-  </div>)
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
