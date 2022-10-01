e07b5e4f901c1b30f3fb68acd54a026011b1b023
unknown <Jossuha@hotmail.fr>
2022-10-01 16:42:49 +0200

    ajout homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage"
 
 function App() {
-  return <div className="App"></div>;
+  return (<div className="App">
+  <Routes>
+    <Route path="/" element={<Homepage />} />
+  </Routes>
+
+  </div>)
 }
 
 export default App;
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,10 @@
+function Homepage(){
+    return(
+        <div>
+            test
+        </div>
+    )
+}
+
+
+export default Homepage
