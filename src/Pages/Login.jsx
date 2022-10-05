3d95e2a41d5a676bf5c881b4558dce1c05718d15
WilsonF78 <fortes.wilson78@gmail.com>
2022-10-05 21:53:27 +0200

    Login page Done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,17 +3,25 @@ import "../Sass/login.scss";
 function Login() {
   return (
     <div className="login">
-      <h1 className="heading">Login</h1>
-      <input type="email" placeholder="email" className="email" required />
-      <input
-        type="password"
-        placeholder="password"
-        className="passeword"
-        required
-      />
+      <h1 className="heading">Already Have an account?</h1>
+      <div className="login-connection">
+        <input type="email" placeholder="email" className="email" required />
+        <input
+          type="password"
+          placeholder="password"
+          className="password"
+          required
+        />
 
-      <div className="login-btn">Login</div>
+        <div className="login-btn">Login</div>
+      </div>
       <p className="text">New Here??</p>
+      <p className="text-twoo">
+        <a href="" className="text-three">
+          Signup
+        </a>{" "}
+        and create an account TODAY!
+      </p>
     </div>
   );
 }
