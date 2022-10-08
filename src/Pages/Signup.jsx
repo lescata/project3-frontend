4e5d75a4c4cf469e8235a167405f3dc0f8852c82
WilsonF78 <fortes.wilson78@gmail.com>
2022-10-08 12:05:56 +0200

    Signup Done

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,39 +1,37 @@
+import "../Sass/signup.scss";
+
 function Signup() {
   return (
-    <div classeName="signup">
+    <div className="signup">
       <h1 className="heading">JOIN US today</h1>
-
-      <input
-        type="firstName"
-        placeholder="First Name"
-        className="firstName"
-        required
-      />
-
-      <input
-        type="lastName"
-        placeholder="Last Name"
-        className="lastName"
-        required
-      />
-
-      <input type="email" placeholder="Email" className="email" required />
-
-      <input
-        type="password"
-        placeholder="Password"
-        className="password"
-        required
-      />
-
-      <input
-        type="passwordConfirm"
-        placeholder="Password Confirmation"
-        className="passwordConfirm"
-        required
-      />
-
-      <div className="login-btn">Signup!</div>
+      <div className="signup-info">
+        <input
+          type="firstName"
+          placeholder="First Name"
+          className="firstName"
+          required
+        />
+        <input
+          type="lastName"
+          placeholder="Last Name"
+          className="lastName"
+          required
+        />
+        <input type="email" placeholder="Email" className="email" required />
+        <input
+          type="password"
+          placeholder="Password"
+          className="password"
+          required
+        />
+        <input
+          type="passwordConfirm"
+          placeholder="Password Confirmation"
+          className="passwordConfirm"
+          required
+        />
+        <div className="login-btn">Signup!</div>{" "}
+      </div>
     </div>
   );
 }
