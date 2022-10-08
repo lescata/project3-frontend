9fd2b51a768aecf16483048f85b4922bc3cb887b
WilsonF78 <fortes.wilson78@gmail.com>
2022-10-08 12:32:31 +0200

    Profile En cour

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,55 +1,63 @@
+import "../Sass/profile.scss";
+
 function Profile() {
   return (
     <div className="contactDetails">
+      <p>
+        <a href="">Commandes</a>
+        <a href="">Profile</a>
+      </p>
       <h1 className="heading">YOUR CONTACT DETAILS</h1>
-      <input
-        type="firstName"
-        placeholder="First Name"
-        className="firstName"
-        required
-      />
+      <div className="info-Details">
+        <input
+          type="firstName"
+          placeholder="First Name"
+          className="firstName"
+          required
+        />
 
-      <input
-        type="lastName"
-        placeholder="Last Name"
-        className="lastName"
-        required
-      />
+        <input
+          type="lastName"
+          placeholder="Last Name"
+          className="lastName"
+          required
+        />
 
-      <input type="email" placeholder="Email" className="email" required />
+        <input type="email" placeholder="Email" className="email" required />
 
-      <input
-        type="password"
-        placeholder="Password"
-        className="password"
-        required
-      />
+        <input
+          type="password"
+          placeholder="Password"
+          className="password"
+          required
+        />
 
-      <div className="address">
-        <h1 className="heading">ADDRESS</h1>
-        <div className="street">
+        <div className="address">
+          <h1 className="heading">ADDRESS</h1>
+          <div className="street">
+            <input
+              type="number"
+              placeholder="Number"
+              className="number"
+              required
+            />
+            <input
+              type="streetName"
+              placeholder="Street Name"
+              className="streetName"
+              required
+            />
+          </div>
+
+          <input type="city" placeholder="City" className="city" required />
+
           <input
-            type="number"
-            placeholder="Number"
-            className="number"
-            required
-          />
-          <input
-            type="streetName"
-            placeholder="Street Name"
-            className="streetName"
+            type="country"
+            placeholder="Country"
+            className="country"
             required
           />
         </div>
-
-        <input type="city" placeholder="City" className="city" required />
-
-        <input
-          type="country"
-          placeholder="Country"
-          className="country"
-          required
-        />
       </div>
     </div>
   );
