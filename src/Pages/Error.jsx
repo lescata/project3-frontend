5f9d891daeb82d3683b4fc525325e3798e0b4aed
WilsonF78 <fortes.wilson78@gmail.com>
2022-10-12 21:36:46 +0200

    About/Contact Done

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,16 @@
+import "../Sass/error.scss";
+
 function Error() {
-  return <div>Error</div>;
+  return (
+    <div className="error">
+      <div className="imageError">
+        <img
+          src="https://res.cloudinary.com/dq7egs1s7/image/upload/v1665602889/image-erreur-404_ztjkkv.png"
+          alt="imgError404"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Error;
