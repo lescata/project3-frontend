4b41e1b95dc68c192af15f8490a079510be0db1f
WilsonF78 <fortes.wilson78@gmail.com>
2022-10-15 16:06:09 +0200

    Rejected/success Done

--- src/Pages/Rejected.jsx
+++ src/Pages/Rejected.jsx
@@ -1,5 +1,18 @@
+import "../Sass/rejected.scss";
+
 function Rejected() {
-  return <div>Rejected</div>;
+  return (
+    <div className="rejected">
+      <div className="rejectedImg">
+        <h1>Rejected</h1>
+        <img
+          src="https://res.cloudinary.com/dq7egs1s7/image/upload/v1665829065/image_20_naefxf.png"
+          alt="ironHackRed"
+        />
+        <div className="tryAgainLink">Try Again</div>
+      </div>
+    </div>
+  );
 }
 
 export default Rejected;
--- src/Pages/Success.jsx
+++ src/Pages/Success.jsx
@@ -1,5 +1,25 @@
+import "../Sass/success.scss";
+
 function Success() {
-  return <div>Success</div>;
+  return (
+    <div className="success">
+      <div className="successImg">
+        <h1>Your order has been received </h1>
+        <img
+          src="https://res.cloudinary.com/dq7egs1s7/image/upload/v1665830950/image_16_kmofyp.png"
+          alt="image"
+        />
+        <div className="successText">
+          <p>Thank you for your Purchase!</p>
+          <p>your order ID is :{}</p>
+          <p>You wil receive an order confirmation email with order details.</p>
+        </div>
+        <div className="successLink">
+          <span>Continue Shopping</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Success;
